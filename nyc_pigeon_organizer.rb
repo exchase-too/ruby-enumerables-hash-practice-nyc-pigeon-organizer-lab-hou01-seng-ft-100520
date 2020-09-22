c85848a5f94e32c403e032ba6e2983e5674437d4
Eric Smith <eric.c.smith@sbcglobal.net>
2020-09-22 19:06:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,10 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
 #    binding.pry
     value.each do |inner_key, names|
-      binding.pry
+      #binding.pry
+      names.each do |name|
+        binding.pry
+      end
     end
     final_array
   end
